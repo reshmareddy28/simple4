00d1f2d8326263282781d81bc02ec158ca7cd9bc
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 16:36:31 +0530

    Create countchar.java

---
+++ countchar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0,i=0;
+		for(i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				count++;
+			}
+			
+		}
+		System.out.println(""+count);
+	}
+
+}
