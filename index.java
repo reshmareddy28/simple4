40be11ba04315fa01addfe42b592433d4dad036f
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 16:30:34 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc. nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("\n"+a[i]+" "+i);
+		}
+	}
+}
