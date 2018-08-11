cbeeb7095403480758386c874f96c2b0f1beee00
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 16:09:59 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int pro=a*b;
+		int div=pro/c;
+		System.out.println(div);
+	}
+}
