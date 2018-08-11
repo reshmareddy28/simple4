f78a13cbcbaccbdbf391a29739022b50267ebc37
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 16:33:22 +0530

    Create houmin.java

---
+++ houmin.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int h1=sc.nextInt();
+		int m1=sc.nextInt();
+		int h2=sc.nextInt();
+		int m2=sc.nextInt();
+		int h3=h1-h2;
+		int m3=m1-m2;
+		System.out.println(h3+" "+m3);
+		
+	}
+}
