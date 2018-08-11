d63750799928493cef7f81a863a8feaf2ba011f2
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 16:38:57 +0530

    Create countlin.java

---
+++ countlin.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String a;
+      int count=0;
+      Scanner s=new Scanner(System.in);
+      a=s.nextLine();
+     String b[]=a.split("\\.");
+     for(int i=0;i<b.length;i++){
+         count++;
+     }
+     System.out.println(count);
+    }
+
+
+}
